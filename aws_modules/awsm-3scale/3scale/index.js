7886014cbaa3bff7c4f30ba18a4ed202e8bf8ee1
jerzyn <andrew.jarzyna@gmail.com>
2015-11-06 13:27:43 +0100

    Quickfix module settings

--- aws_modules/awsm-3scale/3scale/index.js
+++ aws_modules/awsm-3scale/3scale/index.js
@@ -9,23 +9,10 @@ module.exports.run = function(event, context, cb) {
   return cb(null, action(event, context));
 };
 
-function getMethodName(path, method) {
-    if (!path || !method) return 'hits';
-    var pathFrag = path.replace(/^\/|\/$/g, '').replace(/\//g, '_');
-    return pathFrag + '_' + method;
-};
-
 var action = function(event, context) {
-	var method = getMethodName(event.resourcePath, event.httpMethod);
-	if (event.user_key) {
-    threescale.authenticate(event.user_key, method, function(err, res) {
-      if (err) {
-          context.fail(err);
-      } else {
-          context.succeed(true);
-      }
-    });
-	} else {
-	  context.fail('user_key missing');
-	}
+	if (threescale.authenticate(event.user_key)) {
+      context.succeed({"question": pickAQuestion(), "answer": pickAnAnswer()});
+    } else {
+      context.fail('403, unauthorized');
+    }
 };
